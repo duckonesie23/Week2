987f4f871cad07a7775af9c0acf71add360d513f
duckonesie23 <51180797+duckonesie23@users.noreply.github.com>
2024-09-09 14:58:59 -0400

    fda

--- SumofCubes.java
+++ SumofCubes.java
@@ -4,9 +4,12 @@ public class SumofCubes{
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        Double num= input.nextDouble();
+        int num= input.nextInt();
         int sum = 0;
-        Double thing = num;
+        int ones = num%10;
+        int tens = ((num%10) - ones) /10;
+        System.out.print(ones);
+        System.out.print(tens);
         //System.out.print(num);
         /*for(int i=0;thing>10;++i){
             Double temp = thing%(Math.pow(10,i+1));
